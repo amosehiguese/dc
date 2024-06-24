66c8b24eef055772b632deaec18cacba2a3754c9
amosehiguese <amosehiguese@gmail.com>
2024-06-24 20:57:30 +0100

    feat: add chat events

---
+++ modules/dc/events/events.go
@@ -0,0 +1,22 @@
+package events
+
+const (
+	MESSAGE            string = "MESSAGE"
+	JOIN_NEW_CHAT      string = "JOIN_NEW_CHAT"
+	MESSAGE_SEEN       string = "MESSAGE_SEEN"
+	MESSAGE_DELETE     string = "MESSAGE_DELETE"
+	NEW_GROUP          string = "NEW_GROUP"
+	GROUP_UPDATE       string = "GROUP_UPDATE"
+	DELETE_CHAT        string = "DELETE_CHAT"
+	USER_TYPING        string = "USER_TYPING"
+	UNREAD_MESSAGE     string = "UNREAD_MESSAGE"
+	NEW_DOCTOR_REQUEST string = "NEW_DOCTOR_REQUEST"
+	NEW_MEMBER_REQUEST string = "NEW_MEMBER_REQUEST"
+	MEMBER_REMOVED     string = "MEMBER_REMOVED"
+	ONLINE             string = "ONLINE"
+	OFFLINE            string = "OFFLINE"
+	MESSAGE_EDIT       string = "MESSAGE_EDIT"
+	ONLINE_USERS       string = "ONLINE_USERS"
+	NEW_REACTION       string = "NEW_REACTION"
+	DELETE_REACTION    string = "DELETE_REACTION"
+)
